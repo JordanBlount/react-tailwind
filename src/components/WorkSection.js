21a6866e0d4f44aebff3db9f7a6300469328baff
Jordan Blount <77266395+JordanBlount@users.noreply.github.com>
2021-11-22 22:45:00 -0500

    Fix spacing for work section on mobile

--- src/components/WorkSection.js
+++ src/components/WorkSection.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { FaApple } from 'react-icons/fa'
+import { FaApple, FaGithub } from 'react-icons/fa'
 import { SiLoom } from 'react-icons/si'
-import { Link } from 'react-router-dom'
 import WorkCard from '../components/WorkCard'
 
 const WorkSection = () => {
     return (
-        <section id="work" className="bg-white min-h-screen pt-10">
+        // TODO: Fix the spacing on larger screens. There is too much white space under the last WorkCard
+        <section id="work" className="bg-white pt-10 pb-10 md:pb-0 min-h-screen">
             <div className="mx-3 lg:container lg:mx-auto">
                 <h2 className="text-2xl text-center capitalize text-blue-500 font-medium mb-10">My work</h2>
                 <div className="flex flex-col items-center space-y-10">
                     <WorkCard company="Apple" job="Senior Software Enginner" icon={<FaApple className="w-12 h-12" />} />
+                    <WorkCard company="Github" job="Software Engineer 2" icon={<FaGithub className="w-12 h-12" />} />
                     <WorkCard company="Loom" job="Software Engineer, Entry-Level" icon={<SiLoom className="w-12 h-12" />} />
                 </div>
             </div>
